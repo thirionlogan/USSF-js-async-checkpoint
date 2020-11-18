d38c57a4ffdaa69992e60639b4612069ddd9e576
Logan Thirion <thirionlogan@gmail.com>
2020-11-18 14:12:02 -0600

    add chalk for terminal output

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
 const fs = require("fs");
+const chalk = require("chalk");
 
 const inputFileName = process.argv[2] ? process.argv[2] : "input.txt";
 const outputFileName = process.argv[3] ? process.argv[3] : "output.txt";
@@ -10,11 +11,13 @@ fs.readFile(inputFileName, (err, data) => {
     .split("\n")
     .map((str) => str.trim())
     .forEach((name) => {
+      console.log(chalk.black.bgYellow(`Fetching data for ${name}`));
       fetch(`http://pokeapi.co/api/v2/pokemon/${name}`)
-        .then((res) => {
-          return res.json();
-        })
+        .then((res) => res.json())
         .then((pokeData) => {
+          console.log(
+            chalk.black.bgGreen(`Received data for ${pokeData.name}`)
+          );
           return `${pokeData.name}: ${pokeData.types
             .map((type) => type.type.name)
             .join(",")}\n`;
@@ -24,6 +27,6 @@ fs.readFile(inputFileName, (err, data) => {
             if (err) throw new Error(err);
           });
         })
-        .catch((err) => console.error(err));
+        .catch((err) => console.error(chalk.black.bgRed(err)));
     });
 });
